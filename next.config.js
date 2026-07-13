7056ac45aaa67c31ce34b4d6157689d163ff326e
Vishal Dasari <vishaldasari3@gmail.com>
2026-07-13 19:44:10 -0400

    added new post and refactored blog.html
    added new post and refactored blog.html

--- next.config.js
+++ next.config.js
@@ -10,7 +10,9 @@ const nextConfig = {
         { source: '/tech-stack', destination: '/tech-stack.html' },
         { source: '/contact', destination: '/contact.html' },
         { source: '/blog', destination: '/blog.html' },
-        { source: '/blog-post', destination: '/blog-post.html' },
+        { source: '/blog/ai-in-2026', destination: '/blogs/blog-ai-in-2026.html' },
+        { source: '/blog/a-decade-of-moving', destination: '/blogs/blog-a-decade-of-moving.html' },
+        { source: '/blog/ai-agents-reshaping-tech', destination: '/blogs/blog-ai-agents-reshaping-tech.html' },
       ],
     };
   },
