afa3ecf5e37585443ada17c4aa52a92c0c8038b8
Vishal Dasari <vishaldasari3@gmail.com>
2026-07-13 14:57:07 -0400

    deleted files
    deleted filed

--- app/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
-      <h1>404</h1>
-      <p>Page not found. <a href="/">Go home</a></p>
-    </div>
-  );
-}
